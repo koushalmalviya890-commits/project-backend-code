85dc5842b56106cc8e83d124e50f46b998e9852b
cumma-code-25 <cummatech@gmail.com>
2026-02-01 11:13:25 +0530

    duplicate middleware folder

--- src/routes/fetchStartupsRoutes.js
+++ src/routes/fetchStartupsRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middlewares/authMiddleware");
+const {protect} = require("../middleware/authMiddleware");
 
 const {
   getCustomers,
