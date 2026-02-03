d29ed8a80baf0687c96a0c955733354f2f4ba6e4
Gunjan7009 <gunjandixit1712@gmail.com>
2026-02-03 11:10:52 +0530

    chechuser

--- src/routes/checkUserRoutes.js
+++ src/routes/checkUserRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middleware/authMiddleware"); // Import Protect
+const { protect } = require("../middleware/authMiddleware"); 
 
 const { checkUserController } = require("../controllers/checkUserController");
 router.post("/checkuser", protect, checkUserController);
